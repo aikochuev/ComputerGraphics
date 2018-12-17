830a581e2067160d2af0e21d755b79f18e2da027
Ekaterina Shiryaeva <ekaterina.shiryaeva.alexandrovna@intel.com>
2018-12-18 00:59:24 +0300

    Added button controls for different methods

--- ComputerGraphics/ComputerGraphics/Form1.cs
+++ ComputerGraphics/ComputerGraphics/Form1.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ComputerGraphics
@@ -13,6 +15,15 @@ namespace ComputerGraphics
         {
             pictureBox1.Image = bmp1;
             pictureBox2.Image = bmp2;
+        }
+
+        void ComputeMetrics()
+        {
+            FileInfo file1 = new FileInfo(@".\\bmp1_res.png");
+            FileInfo file2 = new FileInfo(@".\\bmp2_res.png");
+
+            label4.Text = Math.Round((double)file1.Length/1024).ToString()+" B";
+            label5.Text = Math.Round((double)file2.Length/1024).ToString()+" B";
         }
 
         public Form1()
@@ -44,11 +55,21 @@ namespace ComputerGraphics
             //bmp1 = new Bitmap(PgmImage.MakeBitmap(pgm.pgmImage, 1));
 
             bmp1 = new Bitmap(FormatReader.ReadBitmapFromPpm(files[0]));
-            bmp2 = bmp1;
             //fs.Close();
+            Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //bmp2.Dispose();
+            bmp2 = bmp1;
+            bmp1.Save(".\\bmp1_res.png", ImageFormat.Png);
             CompressionAlgorithm.RunLengthEncodeBitmap(bmp2);
             //bmp1.Save("D:\\Documents\\Downloads\\rgb8bit\\flower_foveon_res.ppm");
+            bmp1.Save(".\\bmp2_res.png", ImageFormat.Png);
+
             Clear();
+            ComputeMetrics();
         }
     }
 }
